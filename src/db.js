36ba1d7178cca13b173356afd6b24d29495dd121
Yegor Kashirin <egor4815162342@yandex.ru>
2021-11-18 13:41:06 +0300

    make it better

---
+++ src/db.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+module.exports = {
+  connect:DB_HOST => {
+    mongoose.set('useNewUrlParser',true);
+    mongoose.set('useFindAndModify',false);
+    mongoose.set('useCreateIndex',true);
+    mongoose.set('useUnifiedTopology',true);
+    mongoose.connect(DB_HOST);
+    mongoose.connection.on('error',err =>{
+      console.log(err);
+      console.log(
+        "MongoDB connection error.PLease make sure MongoDB is running."
+      )
+      process.exit();
+    })
+  },
+  close: () => {
+    mongoose.connection.close();
+  }
+}
